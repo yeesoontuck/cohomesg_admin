b83227653a443c514afc2b908314689eef0e1b23
Yee <yeesoontuck@gmail.com>
2025-11-05 14:31:11 +0800

    fix for npm run dev, valet link differs from code directory

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            detectTls: 'cohomesg.test', // only affects npm run dev, because valet link differs from directory name
         }),
         tailwindcss(),
     ],
