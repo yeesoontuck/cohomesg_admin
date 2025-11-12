421e18d2805dfd26d05bda47ddf751fe49ad92d2
Yee <yeesoontuck@gmail.com>
2025-11-12 14:22:54 +0800

    make banner visible in mobile, only hide email

--- resources/views/site/banner.blade.php
+++ resources/views/site/banner.blade.php
@@ -1,4 +1,4 @@
-<div class="hidden md:block bg-[#3C3C3C] w-full text-white">
+<div class="bg-[#3C3C3C] w-full text-white">
     <div class="flex justify-between max-w-[1120px] mx-auto px-8 py-2">
         <div class="flex items-center text-[16px]">
             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="white"
@@ -11,13 +11,15 @@
             </svg>
             <span class="mr-6">+65 8809 3036</span>
 
-            <svg xmlns="http://www.w3.org/2000/svg" id="icon-mail2" width="11" height="11" viewBox="0 0 32 32"
-                fill="white" class="mr-3">
-                <path
-                    d="M26.667 0h-21.333c-2.934 0-5.334 2.4-5.334 5.334v21.332c0 2.936 2.4 5.334 5.334 5.334h21.333c2.934 0 5.333-2.398 5.333-5.334v-21.332c0-2.934-2.399-5.334-5.333-5.334zM26.667 4c0.25 0 0.486 0.073 0.688 0.198l-11.355 9.388-11.355-9.387c0.202-0.125 0.439-0.198 0.689-0.198h21.333zM5.334 28c-0.060 0-0.119-0.005-0.178-0.013l7.051-9.78-0.914-0.914-7.293 7.293v-19.098l12 14.512 12-14.512v19.098l-7.293-7.293-0.914 0.914 7.051 9.78c-0.058 0.008-0.117 0.013-0.177 0.013h-21.333z">
-                </path>
-            </svg>
-            <span>hello@cohomesg.com</span>
+            <div class="hidden md:flex md:items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" id="icon-mail2" width="11" height="11" viewBox="0 0 32 32"
+                    fill="white" class="mr-3">
+                    <path
+                        d="M26.667 0h-21.333c-2.934 0-5.334 2.4-5.334 5.334v21.332c0 2.936 2.4 5.334 5.334 5.334h21.333c2.934 0 5.333-2.398 5.333-5.334v-21.332c0-2.934-2.399-5.334-5.333-5.334zM26.667 4c0.25 0 0.486 0.073 0.688 0.198l-11.355 9.388-11.355-9.387c0.202-0.125 0.439-0.198 0.689-0.198h21.333zM5.334 28c-0.060 0-0.119-0.005-0.178-0.013l7.051-9.78-0.914-0.914-7.293 7.293v-19.098l12 14.512 12-14.512v19.098l-7.293-7.293-0.914 0.914 7.051 9.78c-0.058 0.008-0.117 0.013-0.177 0.013h-21.333z">
+                    </path>
+                </svg>
+                <span>hello@cohomesg.com</span>
+            </div>
         </div>
         <div class="flex gap-4">
             <a class="cursor-pointer" href="https://www.facebook.com/cohome.sg" target="_blank" data-type="url"
