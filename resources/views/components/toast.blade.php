e51eaf74a655b142ddc57deaf3d06a14bf2bf0df
Yee <yee@aethel-ai.com>
2025-12-29 02:05:50 +0800

    refactor, without switch-case

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -2,25 +2,6 @@
     'type' => 'success'
 ])
 
-@php
-switch ($type) {
-    case 'success':
-        $type_class = 'bg-success/80';
-        break;
-    case 'info':
-        $type_class = 'bg-info/80';
-        break;
-    case 'warning':
-        $type_class = 'bg-warning/80';
-        break;
-    case 'danger':
-        $type_class = 'bg-danger/80';
-        break;
-}    
-
-@endphp
-
-
 <div 
     x-data="{ show: true }" 
     x-show="show" 
@@ -28,7 +9,7 @@ switch ($type) {
     x-init="setTimeout(() => show = false, 3500)"
     class="fixed top-20 right-4 z-200"
 >
-    <div class="rounded-xl px-6 py-4 shadow-lg text-white font-medium backdrop-blur-sm {{ $type_class }}">
+    <div class="rounded-xl px-6 py-4 shadow-lg text-white font-medium backdrop-blur-sm {{ 'bg-'.$type.'/80' }}">
         {{ $slot }}
     </div>
 </div>
