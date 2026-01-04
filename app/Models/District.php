1fdc44bc8232b85229e54c987676436f9c308d34
Yee <yee@aethel-ai.com>
2026-01-05 00:08:31 +0800

    Audit District and Document models

--- app/Models/District.php
+++ app/Models/District.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use OwenIt\Auditing\Contracts\Auditable;
 use Illuminate\Database\Eloquent\Model;
 
-class District extends Model
+class District extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     protected $fillable = ['id', 'district_name', 'districts_full'];
 
     public function properties()
--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Document extends Model
+class Document extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     // fillable
     protected $fillable = ['template', 'name', 'contents'];
 
