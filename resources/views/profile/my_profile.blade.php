70c5686aa937b48e2ef6a8e165c6e4e79e5037d6
Yee <yee@aethel-ai.com>
2026-01-11 16:29:07 +0800

    Role could be null

--- resources/views/profile/my_profile.blade.php
+++ resources/views/profile/my_profile.blade.php
@@ -39,7 +39,7 @@
                 <label for="role_id" class="w-fit pl-0.5 text-sm">Role</label>
                 <p
                     class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark">
-                    {{ $user->role->name }}
+                    {{ $user->role?->name }}
                 </p>
             </div>
 
