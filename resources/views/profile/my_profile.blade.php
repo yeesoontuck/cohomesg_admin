b9841a652cc175b80e123476e31f7cb1ed4d900c
Yee <yee@aethel-ai.com>
2026-01-11 16:16:58 +0800

    submit button is disabled until all new password rules are met
    remove any validation error messages when user starts typing again @input
    wrap input and error in a div, so that the nextElementSibling will target the error correctly

--- resources/views/profile/my_profile.blade.php
+++ resources/views/profile/my_profile.blade.php
@@ -60,48 +60,55 @@
 
 
         <form action="{{ route('profile.update_password', $user) }}" method="POST"
-            class="p-8 overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
-            @csrf
-            @method('PUT')
-
-            <div x-data="{ showPassword: false }"
-                class="relative mb-4 flex w-full max-w-xs flex-col gap-1 text-on-surface dark:text-on-surface-dark">
-                <label for="current_password"
-                    class="w-fit pl-0.5 text-sm after:ml-0.5 after:text-red-500 after:content-['*']">Current
-                    Password</label>
-                <input id="current_password" :type="showPassword ? 'text' : 'password'"
-                    class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark"
-                    name="current_password" value="" required />
-                <button type="button" class="absolute top-8 right-0 px-3" @click="showPassword = !showPassword"
-                    x-text="showPassword? 'hide' : 'show'" tabindex="-1"></button>
-                @error('current_password')
-                    <div class="text-red-500 text-sm">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div x-data="{
+             x-data="{
                 password: '',
-                showPassword: false,
                 rules: {
                     length: p => p.length >= 8,
                     number: p => /[0-9]/.test(p),
                     lowercase: p => /[a-z]/.test(p),
                     uppercase: p => /[A-Z]/.test(p),
                     special: p => /[!@#$%^&*(),.?\':{}|<>]/.test(p)
+                },
+                get isInvalid() {
+                    return !Object.values(this.rules).every(rule => rule(this.password));
                 }
-            }">
+            }"
+            class="p-8 overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
+            @csrf
+            @method('PUT')
+
+            <div x-data="{ showCurrentPassword: false }"
+                class="relative mb-4 flex w-full max-w-xs flex-col gap-1 text-on-surface dark:text-on-surface-dark">
+                <label for="current_password"
+                    class="w-fit pl-0.5 text-sm after:ml-0.5 after:text-red-500 after:content-['*']">Current
+                    Password</label>
+                <div>
+                    <input @input="$el.nextElementSibling?.remove()" id="current_password" :type="showCurrentPassword ? 'text' : 'password'"
+                        class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark"
+                        name="current_password" value="" required />
+                        @error('current_password')
+                            <div class="text-red-500 text-sm">{{ $message }}</div>
+                        @enderror
+                </div>
+                <button type="button" class="absolute top-8 right-0 px-3" @click="showCurrentPassword = !showCurrentPassword"
+                    x-text="showCurrentPassword? 'hide' : 'show'" tabindex="-1"></button>
+            </div>
+
+            <div x-data="{ showPassword: false }">
                 <div
                     class="relative mb-1 flex w-full max-w-xs flex-col gap-1 text-on-surface dark:text-on-surface-dark">
                     <label for="new_password"
                         class="w-fit pl-0.5 text-sm after:ml-0.5 after:text-red-500 after:content-['*']">New
                         Password</label>
-                    <input id="new_password" :type="showPassword ? 'text' : 'password'" x-model="password" name="new_password"
-                        class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark">
+                    <div>
+                        <input @input="$el.nextElementSibling?.remove()" id="new_password" :type="showPassword ? 'text' : 'password'" x-model="password" name="new_password"
+                            class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark">
+                            @error('new_password')
+                                <div class="text-red-500 text-sm">{{ $message }}</div>
+                            @enderror
+                    </div>
                     <button type="button" @click="showPassword = !showPassword" x-text="showPassword ? 'hide' : 'show'"
                         class="absolute top-8 right-0 px-3" tabindex="-1"></button>
-                    @error('new_password')
-                        <div class="text-red-500 text-sm">{{ $message }}</div>
-                    @enderror
                 </div>
 
                 <ul class="mt-1 space-y-1 text-sm">
@@ -137,7 +144,7 @@
 
             <div class="mt-8 flex justify-between">
                 <div class="flex gap-2">
-                    <button type="submit" class="btn-secondary">Update Password</button>
+                    <button :disabled="isInvalid" type="submit" class="btn-secondary" :class="isInvalid? 'opacity-50' : ''">Update Password</button>
                 </div>
             </div>
         </form>
