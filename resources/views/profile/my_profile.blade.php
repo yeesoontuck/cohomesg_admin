d9186318ed84aaf92dd9ce3607b7e5edc9198807
Yee <yee@aethel-ai.com>
2026-01-11 16:32:41 +0800

    add height to blank role field

--- resources/views/profile/my_profile.blade.php
+++ resources/views/profile/my_profile.blade.php
@@ -38,7 +38,7 @@
             <div class="relative mb-4 flex w-full max-w-xs flex-col gap-1 text-on-surface dark:text-on-surface-dark">
                 <label for="role_id" class="w-fit pl-0.5 text-sm">Role</label>
                 <p
-                    class="w-full rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark">
+                    class="w-full h-9 rounded-radius border border-outline bg-surface-alt px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 dark:border-outline-dark dark:bg-surface-dark-alt/50 dark:focus-visible:outline-primary-dark">
                     {{ $user->role?->name }}
                 </p>
             </div>
