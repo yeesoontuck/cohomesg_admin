0523c4d0f0d82ee389b98ea2204e51278f9efa31
Yee <yee@aethel-ai.com>
2026-01-13 14:41:48 +0800

    text color

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -16,7 +16,7 @@
         </div>
 
         <div
-            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 text-neutral-900 dark:text-white dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <form action="{{ route('password.update') }}" method="POST" class="space-y-6">
                     @csrf
