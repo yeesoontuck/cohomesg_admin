95556388ba8a71e725cc77ba66fc96d6b2205f9b
Yee <yee@aethel-ai.com>
2026-01-14 22:49:37 +0800

    dark mode

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -10,7 +10,7 @@
 
     <div id="main" class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         
-        <div class="text-lg font-semibold mb-4">Please confirm your password to continue.</div>
+        <div class="text-lg font-semibold mb-4 text-on-surface dark:text-on-surface-dark">Please confirm your password to continue.</div>
 
         <div
             class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
